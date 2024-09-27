b4703ee3d0d7b42a05b8f98bc7971475b6530be1
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-09-28 00:18:43 +0600

    conditions

---
+++ main.js
@@ -0,0 +1,11 @@
+/* */
+//* Conditions
+let firstName = "Adnan";
+let age = 18;
+let isMarried = false;
+
+if (isMarried == true) {
+  console.log(firstName + "is not married");
+} else {
+  console.log(firstName + "is married");
+}
